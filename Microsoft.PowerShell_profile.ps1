95255dd38792cf9bd89b50cdeaf7821203516832
Akimon658 <akimo_greninja@yahoo.co.jp>
2021-10-31 18:22:20 +0900

    add: display branch

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,9 @@
 function prompt() {
+	$branch = git rev-parse --abbrev-ref HEAD
+
 	Write-Host ''
-	Write-Host $PWD -ForegroundColor Green
+	Write-Host $PWD -NoNewline -ForegroundColor Green
+	Write-Host " ($branch)" -ForegroundColor Blue
 	Write-Host '$' -NoNewline -Foregroundcolor Green
 
 	return ' '
